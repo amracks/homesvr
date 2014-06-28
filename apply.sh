87ba1273eb8266a8beeb9abb7ffc3d94caa22fd2
Andrew Marks <amracks@gmail.com>
2014-06-28 08:24:46 -0700

    Added hiera_config paramater to apply command

--- apply.sh
+++ apply.sh
@@ -4,4 +4,4 @@ export SSL_CERT_FILE=/usr/local/lib/ruby/1.9/rubygems/ssl_certs/ca-bundle.pem
 
 git pull
 librarian-puppet update
-puppet apply --modulepath=./modules manifests/init.pp
+puppet apply --modulepath=./modules --hiera_config=conf/hiera.yaml manifests/init.pp
